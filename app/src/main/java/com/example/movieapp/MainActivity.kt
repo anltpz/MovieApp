9027dfecca45c40d9aba5c5baf5aeabfa143e85d
Anıl TOPUZ <aniltopuz@gmail.com>
2022-01-21 14:50:08 +0300

    MainActivity.kt fix

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -1,11 +1,56 @@
 package com.example.movieapp
 
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.movieapp.adapter.TvShowAdapter
+import com.example.movieapp.databinding.ActivityMainBinding
+import com.example.movieapp.viewmodel.TvShowViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+
+/*
+* Android bileşenlerine bağımlılıkları
+*  enjekte edebilmek için
+*  @AndroidEntryPoint annotasyonunu kullanırız.*/
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel:TvShowViewModel by viewModels()
+    private lateinit var tvShowAdapter : TvShowAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+        setupRv()
+    }
+
+    private fun setupRv() {
+       tvShowAdapter= TvShowAdapter()
+        binding.recylerView.apply {
+            adapter=tvShowAdapter
+            layoutManager=LinearLayoutManager(this@MainActivity,LinearLayoutManager.HORIZONTAL,false)
+
+            setHasFixedSize(true)
+        }
+        binding.rvEpisodes.apply {
+            adapter=tvShowAdapter
+            layoutManager=LinearLayoutManager(this@MainActivity,LinearLayoutManager.HORIZONTAL,false)
+
+            setHasFixedSize(true)
+        }
+
+
+
+
+        viewModel.responseTvShow.observe(this,{listTvShow->
+            tvShowAdapter.tvShow=listTvShow
+
+        })
+
+
     }
 }
