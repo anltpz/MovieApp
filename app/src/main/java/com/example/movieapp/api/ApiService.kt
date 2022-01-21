696bd204a8e9eef9382cc5321088e7fe7a216129
Anıl TOPUZ <aniltopuz@gmail.com>
2022-01-21 16:28:33 +0300

    Update

--- app/src/main/java/com/example/movieapp/api/ApiService.kt
+++ app/src/main/java/com/example/movieapp/api/ApiService.kt
@@ -4,7 +4,7 @@ import com.example.movieapp.helper.Constants
 import com.example.movieapp.model.TvShowResponse
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.POST
+
 
 interface ApiService {
     @GET(Constants.END_POINT)
